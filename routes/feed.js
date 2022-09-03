cf69e89228087d17f933c49fa1d36dc816f26350
Farhan <farhanbajwa46@gmail.com>
2022-09-03 16:37:23 +0500

    access post without authorization

--- routes/feed.js
+++ routes/feed.js
@@ -9,7 +9,7 @@ const feedController = require('../controllers/feed')
 router.get('/posts', feedController.getPost);
 router.get('/post/:postId',isAuth, feedController.getPostById)
 router.post('/createPost',isAuth, [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.createPost)
-router.put('/post/:postId', isAuth,[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.updatePost)
+router.put('/post/:postId',[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.updatePost)
 router.delete('/post/:postId',isAuth,feedController.deletePost)
 
 
