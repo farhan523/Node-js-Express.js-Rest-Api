693f15ecdc4183c470bfb3c68de95556c7b5ece2
Farhan <farhanbajwa46@gmail.com>
2022-09-03 16:45:07 +0500

    routes modified

--- routes/feed.js
+++ routes/feed.js
@@ -7,9 +7,9 @@ const { body } = require('express-validator')
 const feedController = require('../controllers/feed')
 
 router.get('/posts', feedController.getPost);
-router.get('/post/:postId',isAuth, feedController.getPostById)
+router.get('/post/:postId', feedController.getPostById)
 router.post('/createPost',isAuth, [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.createPost)
-router.put('/post/:postId',[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.updatePost)
+router.put('/post/:postId', isAuth,[body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.updatePost)
 router.delete('/post/:postId',isAuth,feedController.deletePost)
 
 
